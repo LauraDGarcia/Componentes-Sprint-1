a7db919d22cca0368dd940ffdea1c92b497109d6
LauraDGarcia-Inlaze <l.garcia@inlaze.com>
2025-03-20 15:22:11 -0500

    commit

--- Sprint2/configuration.py
+++ Sprint2/configuration.py
@@ -54,7 +54,7 @@ class campaignspage(BasePage):
     CONFI_PROFILE_DIALOG_PASSWORD_YES = (By.XPATH, "//button[contains(@class,'163')]")
     CONFI_PROFILE_DIALOG_PASSWORD_NOT = (By.XPATH, "//button[contains(@class,'224')]")
     CONFI_PROFILE_CONTACT_LOCATION = (By.XPATH, "/html/body/div[1]/div[2]/div[2]/section/section/section/div/section/div[3]/span/button")
-    CONFI_PROFILE_COUNTRY = (By.XPATH, "/html/body/div[4]/div[3]/form/div[2]/fieldset[1]/label/div/i[2]")
+    CONFI_PROFILE_COUNTRY = (By.XPATH, "/html/body/div[3]/div[3]/form/div[2]/fieldset[1]/label/div/i[2]")
     CONFI_PROFILE_NEW_COUNTRY = (By.XPATH, "//span[contains(.,'Colombia')]")
     CONFI_PROFILE_COUNTRY_CODE = (By.XPATH, "/html/body/div[4]/div[3]/form/div[2]/span/fieldset[1]/label/div/i[2]")
     CONFI_PROFILE_NEW_COUNTRY_CODE = (By.XPATH, "(//span[contains(.,'+57')])[2]")
@@ -102,12 +102,12 @@ class campaignspage(BasePage):
     CONFI_PREFERENCES_AUDIENCE_OBJETIVE_YES = (By.XPATH, "//button[contains(@class,'163')]")
     CONFI_PREFERENCES_AUDIENCE_OBJETIVE_NOT = (By.XPATH, "//button[contains(@class,'224')]")
     #Configuración ayuda
-    CONFIGURATION_HELP = (By.XPATH, "")
-    CONFI_HELP_CALL = (By.XPATH, "")
-    CONFI_HELP_CHAT = (By.XPATH, "")
-    CONFI_HELP_POLICIES_TYC = (By.XPATH, "")
-    CONFI_HELP_POLICIES_PRIVACY_POLICY = (By.XPATH, "")
-    CONFI_HELP_POLICIES_ISO = (By.XPATH, "")
+    CONFIGURATION_HELP = (By.XPATH, "(//span[contains(@class,'73')])[4]")
+    CONFI_HELP_CALL = (By.XPATH, "(//button[contains(@class,'58')])[4]")
+    CONFI_HELP_CHAT = (By.XPATH, "(//button[contains(@class,'58')])[5]")
+    CONFI_HELP_POLICIES_TYC = (By.XPATH, "(//button[contains(@class,'76')])[1]")
+    CONFI_HELP_POLICIES_PRIVACY_POLICY = (By.XPATH, "(//button[contains(@class,'76')])[2]")
+    CONFI_HELP_POLICIES_ISO = (By.XPATH, "(//button[contains(@class,'76')])[3]")
 
     def navigate_home(self):
         self.go_to_page("https://qa-account-commizzion-vm.inlazetest.com/login") #YUna cuenta ya ingresada, si no poner los del login
@@ -142,3 +142,194 @@ class campaignspage(BasePage):
     def select_button_login(self):
         self.wait_for_element(self.BUTTON_LOGIN).click()
 
+    #Ir a configuración
+
+    def select_configuration(self):
+        self.wait_for_element(self.CONFIGURATION).click()
+
+    #Perfil correo
+    def select_CONFI_PROFILE_LOGIN_DATA(self):
+        self.wait_for_element(self.CONFI_PROFILE_LOGIN_DATA).click()
+
+    def select_CONFI_PROFILE_EMAIL(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL).click()
+
+    def select_CONFI_PROFILE_EMAIL_BUTTONCHECK(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_BUTTONCHECK).click()
+
+    def select_CONFI_PROFILE_EMAIL_BUTTONCONTINUE(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_BUTTONCONTINUE).click()
+
+    def select_CONFI_PROFILE_EMAIL_BUTTONCANCE(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_BUTTONCANCEL).click()
+
+    def text_code(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODEONE).click()
+
+    def get_code1(self, code1):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODEONE)
+        element.clear() 
+        element.send_keys(code1)
+
+    def get_code2(self, code2):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODETWO)
+        element.clear() 
+        element.send_keys(code2)
+
+    def get_code3(self, code3):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODETHREE)
+        element.clear() 
+        element.send_keys(code3)
+
+    def get_code4(self, code4):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODEFOUR)
+        element.clear() 
+        element.send_keys(code4)
+
+    def get_code5(self, code5):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODEFIVE)
+        element.clear() 
+        element.send_keys(code5)
+        
+    def get_code6(self, code6):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_CODESIX)
+        element.clear() 
+        element.send_keys(code6)
+
+    def select_CONFI_PROFILE_EMAIL_RESENDCODE(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_RESENDCODE).click()
+
+    def select_CONFI_PROFILE_EMAIL_HAVENT_RECEIVED(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_HAVENT_RECEIVED).click()
+
+    def select_CONFI_PROFILE_EMAIL_BUTTONNEXT(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_BUTTONNEXT).click()
+
+    def select_CONFI_PROFILE_EMAIL_BUTTON_CANCEL(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_BUTTON_CANCEL).click()
+
+    def select_CONFI_PROFILE_NEW_EMAIL(self):
+        self.wait_for_element(self.CONFI_PROFILE_NEW_EMAIL).click()
+
+    def select_CONFI_PROFILE_NEW_EMAIL(self, email):
+        element = self.wait_for_element(self.CONFI_PROFILE_NEW_EMAIL)
+        element.clear() 
+        element.send_keys(email)
+
+    def select_CONFI_PROFILE_CONFIRM_EMAIL(self):
+        self.wait_for_element(self.CONFI_PROFILE_CONFIRM_EMAIL).click()
+
+    def select_CONFI_PROFILE_NEW_EMAIL(self, email):
+        element = self.wait_for_element(self.CONFI_PROFILE_CONFIRM_EMAIL)
+        element.clear() 
+        element.send_keys(email)
+
+    def select_CONFI_PROFILE_SAVE_EMAIL(self):
+        self.wait_for_element(self.CONFI_PROFILE_SAVE_EMAIL).click()
+
+    def select_CONFI_PROFILE_CANCEL_EMAIL(self):
+        self.wait_for_element(self.CONFI_PROFILE_CANCEL_EMAIL).click()
+
+    def text_code_new(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODEONE).click()
+
+    def get_code1_new(self, code1):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODEONE)
+        element.clear() 
+        element.send_keys(code1)
+
+    def get_code2_new(self, code2):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODETWO)
+        element.clear() 
+        element.send_keys(code2)
+
+    def get_code3_new(self, code3):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODETHREE)
+        element.clear() 
+        element.send_keys(code3)
+
+    def get_code4_new(self, code4):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODEFOUR)
+        element.clear() 
+        element.send_keys(code4)
+
+    def get_code5_new(self, code5):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODEFIVE)
+        element.clear() 
+        element.send_keys(code5)
+        
+    def get_code6_new(self, code6):
+        element = self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_CODESIX)
+        element.clear() 
+        element.send_keys(code6)
+
+    def select_CONFI_PROFILE_EMAIL_NEW_RESENDCODE(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_RESENDCODE).click()
+
+    def select_CONFI_PROFILE_EMAIL_NEW_HAVENT_RECEIVED(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_HAVENT_RECEIVED).click()
+
+    def select_CONFI_PROFILE_EMAIL_NEW_BUTTONNEXT(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_BUTTONNEXT).click()
+
+    def select_CONFI_PROFILE_EMAIL_NEW_BUTTON_CANCEL(self):
+        self.wait_for_element(self.CONFI_PROFILE_EMAIL_NEW_BUTTON_CANCEL).click()
+
+    def select_CONFI_PROFILE_DIALOG_EMAIL_YES(self):
+        self.wait_for_element(self.CONFI_PROFILE_DIALOG_EMAIL_YES).click()
+
+    def select_CONFI_PROFILE_DIALOG_EMAIL_NOT(self):
+        self.wait_for_element(self.CONFI_PROFILE_DIALOG_EMAIL_NOT).click()
+
+    #Perfil contraseña
+    def select_CONFI_PROFILE_EDIT_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_EDIT_PASSWORD).click()
+
+    def select_CONFI_PROFILE_CURRENT_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_CURRENT_PASSWORD).click()
+
+    def select_CONFI_PROFILE_CURRENT_PASSWORD_text(self, password):
+        element = self.wait_for_element(self.CONFI_PROFILE_CURRENT_PASSWORD)
+        element.clear() 
+        element.send_keys(password)
+
+    def select_CONFI_PROFILE_NEW_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_NEW_PASSWORD).click()
+
+    def select_CONFI_PROFILE_NEW_PASSWORD_text(self, newpassword):
+        element = self.wait_for_element(self.CONFI_PROFILE_NEW_PASSWORD)
+        element.clear() 
+        element.send_keys(newpassword)   
+
+    def select_CONFI_PROFILE_CONFIRM_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_CONFIRM_PASSWORD).click() 
+
+    def select_CONFI_PROFILE_CONFIRM_PASSWORD_text(self, newpassword):
+        element = self.wait_for_element(self.CONFI_PROFILE_CONFIRM_PASSWORD)
+        element.clear() 
+        element.send_keys(newpassword) 
+
+    def select_CONFI_PROFILE_SAVE_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_SAVE_PASSWORD).click()
+
+    def select_CONFI_PROFILE_CANCEL_PASSWORD(self):
+        self.wait_for_element(self.CONFI_PROFILE_CANCEL_PASSWORD).click()
+
+    def select_CONFI_PROFILE_DIALOG_PASSWORD_YES(self):
+        self.wait_for_element(self.CONFI_PROFILE_DIALOG_PASSWORD_YES).click()
+
+    def select_CONFI_PROFILE_DIALOG_PASSWORD_NOT(self):
+        self.wait_for_element(self.CONFI_PROFILE_DIALOG_PASSWORD_NOT).click()
+
+    #Perfil Contact and Location Information
+    def select_CONFI_PROFILE_CONTACT_LOCATION(self):
+        self.wait_for_element(self.CONFI_PROFILE_CONTACT_LOCATION).click()
+
+    def select_CONFI_PROFILE_COUNTRY(self):
+        self.wait_for_element(self.CONFI_PROFILE_COUNTRY).click()
+
+    def select_CONFI_PROFILE_NEW_COUNTRY(self):
+        self.wait_for_element(self.CONFI_PROFILE_NEW_COUNTRY).click()
+
+    def select_CONFI_PROFILE_COUNTRY_CODE(self):
+        self.wait_for_element(self.CONFI_PROFILE_COUNTRY_CODE).click()
